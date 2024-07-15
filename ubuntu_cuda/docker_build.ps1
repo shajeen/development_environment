da1ea53680f4f21f712d091adbe3670062352a21
Shajeen Ahamed <shajeenahmed@gmail.com>
2024-07-15 19:51:18 +0530

    ubuntu-cuda

--- ubuntu_cuda/docker_build.ps1
+++ ubuntu_cuda/docker_build.ps1
@@ -1,1 +1,5 @@
-docker build --no-cache -t ubuntu-cuda-image . 
+# Define the Docker build command
+$dockerBuildCommand = "docker build . -t ubuntu-cuda --no-cache"
+
+# Execute the Docker build command
+Invoke-Expression -Command $dockerBuildCommand
--- ubuntu_cuda/readme.md
+++ ubuntu_cuda/readme.md
@@ -2,4 +2,4 @@
 powershell -ExecutionPolicy Bypass -File .\docker_build.ps1
 
 ## manual build imagge
-docker build --no-cache -t ubuntu-cuda-image . 
+docker build --no-cache -t ubuntu-cuda . 
